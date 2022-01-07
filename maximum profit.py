c1a581ecb1bcdcf088d94562c1651ded2c45eeae
SANIAL DAS <53612010+sanial2001@users.noreply.github.com>
2022-01-07 17:43:05 +0530

    Create maximum profit.py

---
+++ maximum profit.py
@@ -0,0 +1,15 @@
+def maxProfit(self, k, prices):  
+    if not prices or k == 0:
+        return 0
+    
+    buy = [float(inf)]*k
+    profit = [0]*k
+    
+    for p in prices:
+        buy[0] = min(buy[0], p)
+        profit[0] = max(profit[0], p - buy[0])
+        for i in range(1,k):
+            buy[i] = min(buy[i], p - profit[i-1])
+            profit[i] = max(profit[i], p - buy[i])
+    
+    return profit[-1]
