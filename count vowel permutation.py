a8f7ca649b1895d2498e503f931b18406d090da7
SANIAL DAS <53612010+sanial2001@users.noreply.github.com>
2022-01-17 00:45:32 +0530

    Create count vowel permutation.py

---
+++ count vowel permutation.py
@@ -0,0 +1,7 @@
+class Solution:
+    def countVowelPermutation(self, n: int) -> int:
+        a, e, i, o, u = 1, 1, 1, 1, 1
+        mod = 10**9+7
+        for _ in range(n-1):
+            a, e, i, o, u = e, a+i, a+e+o+u, i+u, a
+        return (a+e+i+o+u)%(mod)
