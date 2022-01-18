476dd1ea19ad047bb2e163c9f1215c6dbec55795
SANIAL DAS <53612010+sanial2001@users.noreply.github.com>
2022-01-18 12:46:17 +0530

    Create atoi.py

---
+++ atoi.py
@@ -0,0 +1,4 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        match = re.match(r'^\s*([+-]?\d+)', s)
+        return min(max((int(match.group(1)) if match else 0), -2**31), 2**31 - 1)
